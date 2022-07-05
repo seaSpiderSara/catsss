3fda8cf071601536ba89405a4a03673b729624ff
seaSpiderSara <am9106@nyu.edu>
2022-07-04 22:56:32 -0400

    marc_controlller

---
+++ ccattt/Assets/A scripts/marcus_controller.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class marcus_controller : MonoBehaviour
+{
+   //bool for activating and deactivating Marcus controller
+    public bool control_status = true;
+
+
+    public float mSpeed = 10f;
+    public Rigidbody2D marcusRB;
+
+    private Vector2 _movement;
+
+    private void Start()
+    {
+
+    }
+
+    private void Update()
+    {
+        if(control_status == true)
+        {
+            _movement.x = Input.GetAxisRaw("Horizontal");
+            _movement.y = Input.GetAxisRaw("Vertical");
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        marcusRB.MovePosition(marcusRB.position + _movement * mSpeed);
+    }
+
+}
